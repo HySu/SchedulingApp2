d16b7f98b2c5958318ad674e32031b01442c0bc9
memo <colorlhs@gmail.com>
2024-11-15 09:25:27 +0900

    first commit

---
+++ src/main/java/com/example/schedulingapp2/SchedulingApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.schedulingapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingApp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/schedulingapp2/SchedulingApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulingapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
